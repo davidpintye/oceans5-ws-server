cc2296064113acade45b3efd1b80852fbbec2d60
andrejmoltok <csokan.andras87@protonmail.ch>
2024-05-25 07:19:20 +0200

    Update dependencies, add nodemon

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const http = require('http');
-const { Server } = require('socket.io');
+const http = require("http");
+const { Server } = require("socket.io");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('WebSocket szerver fut...\n');
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("WebSocket server is running...\n");
 });
 
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST'],
-    },
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
-const userIo = io.of('/user');
+const userIo = io.of("/user");
 let playerArray = [];
 
-userIo.on('connection', (socket) => {
-    socket.on('player-joined', (player) => {
-        player.socketId = socket.id;
-        playerArray.push(player);
-        userIo.emit('user-list', playerArray);
-    })
+userIo.on("connection", (socket) => {
+  socket.on("player-joined", (player) => {
+    player.socketId = socket.id;
+    playerArray.push(player);
+    userIo.emit("user-list", playerArray);
+  });
 
-    socket.on('lobby-chat', (player, message) => {
-        console.log(`Kliens üzenet: ${message}`);
-        userIo.emit('lobby-chat', player, message);
-    });
+  socket.on("lobby-chat", (player, message) => {
+    console.log(`Kliens üzenet: ${message}`);
+    userIo.emit("lobby-chat", player, message);
+  });
 
-    socket.on('disconnect', () => {
-        playerArray = playerArray.filter(player => player.socketId != socket.id);
-        console.log(playerArray);
-        userIo.emit('user-list', playerArray);
-        console.log('Kliens lekapcsolódott');
-    });
+  socket.on("disconnect", () => {
+    playerArray = playerArray.filter((player) => player.socketId != socket.id);
+    console.log(playerArray);
+    userIo.emit("user-list", playerArray);
+    console.log("Kliens lekapcsolódott");
+  });
 });
 
 server.listen(3001, () => {
-    console.log('WebSocket szerver fut a 3001-es porton.');
+  console.log("WebSocket szerver fut a 3001-es porton.");
 });
