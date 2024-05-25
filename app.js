ce23838c5807029ac3b17f73c95a2943a5285be3
andrejmoltok <csokan.andras87@protonmail.ch>
2024-05-25 07:54:19 +0200

    O5T-35 Add: basic socket server

--- app.js
+++ app.js
@@ -13,29 +13,6 @@ const io = new Server(server, {
   },
 });
 
-const userIo = io.of("/user");
-let playerArray = [];
-
-userIo.on("connection", (socket) => {
-  socket.on("player-joined", (player) => {
-    player.socketId = socket.id;
-    playerArray.push(player);
-    userIo.emit("user-list", playerArray);
-  });
-
-  socket.on("lobby-chat", (player, message) => {
-    console.log(`Kliens üzenet: ${message}`);
-    userIo.emit("lobby-chat", player, message);
-  });
-
-  socket.on("disconnect", () => {
-    playerArray = playerArray.filter((player) => player.socketId != socket.id);
-    console.log(playerArray);
-    userIo.emit("user-list", playerArray);
-    console.log("Kliens lekapcsolódott");
-  });
-});
-
 server.listen(3001, () => {
   console.log("WebSocket szerver fut a 3001-es porton.");
 });
